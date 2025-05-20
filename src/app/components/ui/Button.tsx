0369cc3f0f9839aaff240e3ab59e8bc12e44c752
Okeze Kingsley <okezekingsley18@gmail.com>
2025-05-20 15:23:48 +0100

    button ui changes

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Button({children, className, ...props}: {children:React.ReactNode, className:string; style? : any}) {
+export default function Button({children, className, ...props}: {children:React.ReactNode, className:string; style? : React.CSSProperties}) {
   return (
  <button className={className} type='button' {...props}>
     {children}
